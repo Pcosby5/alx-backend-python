c94a4062f6d7b66cc393cf9df2cad8f85bffa41c
Pcosby5 <pcosby50@gmail.com>
2024-05-26 19:23:18 +0200

    test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,31 +1,53 @@
 #!/usr/bin/env python3
-"""
-Unit tests for client module.
-"""
+"""Integration testing on GithubOrgClient"""
 
 import unittest
-from parameterized import parameterized
+from unittest.mock import patch
+from parameterized import parameterized_class #type: ignore
+from fixtures import TEST_PAYLOAD
 from client import GithubOrgClient
 
 
-class TestGithubOrgClient(unittest.TestCase):
-    """
-    Test cases for GithubOrgClient class.
-    """
+@parameterized_class(
+    ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
+    TEST_PAYLOAD
+)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Set up class method to start patching."""
+        cls.get_patcher = patch('requests.get')
+        cls.mock_get = cls.get_patcher.start()
 
-    @parameterized.expand([
-        ({"license": {"key": "my_license"}}, "my_license", True),
-        ({"license": {"key": "other_license"}}, "my_license", False),
-    ])
-    def test_has_license(self, repo, license_key, expected_has_license):
-        """
-        Test that GithubOrgClient.has_license returns the correct value.
-        """
-        result = GithubOrgClient.has_license(repo, license_key)
+        cls.mock_get.side_effect = lambda url: cls.MockResponse(
+            url, cls.org_payload, cls.repos_payload)
 
-        print(f"Test: repo={repo}, license_key={license_key}, "
-              f"expected_has_license={expected_has_license}, result={result}")
-        self.assertEqual(result, expected_has_license)
+    @classmethod
+    def tearDownClass(cls):
+        """Tear down class method to stop patching."""
+        cls.get_patcher.stop()
+
+    class MockResponse:
+        def __init__(self, url, org_payload, repos_payload):
+            self.url = url
+            self.org_payload = org_payload
+            self.repos_payload = repos_payload
+
+        def json(self):
+            if self.url == 'https://api.github.com/orgs/google':
+                return self.org_payload
+            if self.url == 'https://api.github.com/orgs/google/repos':
+                return self.repos_payload
+
+    def test_public_repos(self):
+        """Test public_repos method."""
+        client = GithubOrgClient('google')
+        self.assertEqual(client.public_repos(), self.expected_repos)
+
+    def test_public_repos_with_license(self):
+        """Test public_repos method with license filter."""
+        client = GithubOrgClient('google')
+        self.assertEqual(client.public_repos('apache-2.0'), self.apache2_repos)
 
 
 if __name__ == "__main__":
